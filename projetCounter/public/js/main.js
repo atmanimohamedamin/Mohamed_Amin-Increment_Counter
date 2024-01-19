abdd073ce507390c99a462b01b20f55d0fb43c78
atmanimohamedamin <atmanii.mohamedamin@gmail.com>
2024-01-19 05:21:59 -0800

    ended

--- projetCounter/public/js/main.js
+++ projetCounter/public/js/main.js
@@ -11,8 +11,29 @@ function updated() {
 }
 
 
+let counts2 = setInterval(updated2);
+let upto2 = 0;
+function updated2() {
+    let count = document.querySelectorAll(".counter2");
+    count.forEach(element => {
+        element.innerHTML = ++upto2;
+        if (upto2 === 5000) {
+            clearInterval(counts2);
+        }
+    });
+}
 
-
+let counts3 = setInterval(updated3);
+let upto3 = 0;
+function updated3() {
+    let count = document.querySelectorAll(".counter3");
+    count.forEach(element => {
+        element.innerHTML = ++upto3;
+        if (upto3 === 7500) {
+            clearInterval(counts3);
+        }
+    });
+}
 
 
 
